3ea4018979a6ce637c45edf6a3e89cdb7f2d17ff
unknown <anunyapornt58@email.nu.ac.th>
2018-01-22 22:31:08 +0700

    firt commit

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 import socket
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5001
+
+def fac(n):
+   if n == 1:
+       return n
+   else:
+       return n*fac(n-1)
+
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+sock.bind((UDP_IP, UDP_PORT))    
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    result = fac(int (data))
+    print "received data:", data
+    print "Result:", result
+    sock.sendto(str(result), addr)
+
+
